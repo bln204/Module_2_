bce087e06bdfd6c91e1a236e06016fda45968bdf
bln204 <lenambui3@gmail.com>
2024-06-08 15:19:37 +0700

    Kiểm tra năm nhuận

---
+++ src/thuc_hanh/LeapYearCalculator.java
@@ -0,0 +1,26 @@
+package thuc_hanh;
+
+import java.util.Scanner;
+
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Nhập số năm: ");
+        year = scanner.nextInt();
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    System.out.printf("%d là năm nhuận", year);
+                } else {
+                    System.out.printf("%d không là năm nhuận", year);
+                }
+            } else {
+                System.out.printf("%d là năm nhuận", year);
+            }
+        } else {
+            System.out.printf("%d không là năm nhuận", year);
+        }
+    }
+}
+
