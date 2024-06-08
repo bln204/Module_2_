9e46ed50e0e564ca15902e620ab4ca9f014a64ce
bln204 <lenambui3@gmail.com>
2024-06-08 14:29:48 +0700

    Khai báo và sử dụng biến

---
+++ src/thuc_hanh/Variable_DatatypeName.java
@@ -0,0 +1,19 @@
+package thuc_hanh;
+
+public class Variable_DatatypeName {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 20.5f;
+        double c = 30.5;
+        boolean d = true;
+        char e = 'c';
+        String s = "Hà Nội";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(s);
+
+    }
+}
