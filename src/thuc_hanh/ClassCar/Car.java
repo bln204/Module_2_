31342192de8bfdc244563a07e36f300f7051c317
bln204 <lenambui3@gmail.com>
2024-06-19 19:08:55 +0700

    Thực hành Static Property

---
+++ src/thuc_hanh/ClassCar/Car.java
@@ -0,0 +1,12 @@
+package thuc_hanh.ClassCar;
+
+public class Car {
+    private String name;
+    private String engine;
+    public static int numberOfCars;
+    public Car(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars++;
+    }
+}
