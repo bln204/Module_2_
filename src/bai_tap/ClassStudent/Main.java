993ad6edab5e441faecfbb3852f8103a7a4a118f
bln204 <lenambui3@gmail.com>
2024-06-19 21:01:26 +0700

    Luyện tập Access Modifier

---
+++ src/bai_tap/ClassStudent/Main.java
@@ -0,0 +1,10 @@
+package bai_tap.ClassStudent;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.setName("Nam");
+        student.setClasses("C03");
+    }
+}
---
+++ src/bai_tap/ClassStudent/Student.java
@@ -0,0 +1,16 @@
+package bai_tap.ClassStudent;
+
+public class Student {
+    private String name = "Jonh";
+    private String Class = "C02";
+
+    public Student (){
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+    public void setClasses (String Class) {
+        this.Class = Class;
+    }
+}
